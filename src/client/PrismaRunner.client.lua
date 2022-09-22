8129dd66fd718b5a775a98282095622808d16a92
reimakesgames <reimakesgames.dev@gmail.com>
2022-09-22 21:10:16 +0800

    prisma fix

---
+++ src/client/PrismaRunner.client.lua
@@ -0,0 +1,6 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Packages = ReplicatedStorage:WaitForChild("Packages")
+local Prisma = Packages:WaitForChild("prisma")
+
+print("wth")
+require(Prisma)
---
+++ src/server/PrismaRunner.server.lua
@@ -0,0 +1,5 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Packages = ReplicatedStorage:WaitForChild("Packages")
+local Prisma = Packages:WaitForChild("prisma")
+
+require(Prisma)
