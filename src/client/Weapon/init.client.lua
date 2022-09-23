74027562d2cac7fec76dddeb6810169a794e7e99
reimakesgames <reimakesgames.dev@gmail.com>
2022-09-24 01:05:27 +0800

    Weapon script optimization

--- src/client/Weapon/init.client.lua
+++ src/client/Weapon/init.client.lua
@@ -1,32 +1,31 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 
+local Assets = ReplicatedStorage:WaitForChild("Assets")
+
+local Gameplay = Assets:WaitForChild("Gameplay")
+local Environment = Gameplay:WaitForChild("Environment")
+
 local Camera = workspace.CurrentCamera
+local Viewmodel = ReplicatedStorage:WaitForChild("v_UMP45"):Clone()
 
 local Caster = require(script.Caster)
+local CastEffects = require(script.CastEffects)
 
-local function CreatePair(Origin, Stop)
-	local Start = workspace.Start:Clone()
-	local End = workspace.End:Clone()
-
-	Start.Position = Origin
-	End.Position = Stop
-
-	Start.Parent = workspace
-	End.Parent = workspace
-	Start.Attachment.Beam.Attachment1 = End.Attachment
-end
-
-local function Cast()
-	local Result: RaycastResult = Caster:Cast(Camera.CFrame.Position, Camera.CFrame.LookVector * 1024)
-
+local function WeaponFire(Origin: Vector3, Direction: Vector3)
+	local Result: RaycastResult = Caster:Cast(Origin, Direction * 1024)
+	
 	if not Result then
+		CastEffects:CreateFakeTracer(Viewmodel["UMP-45"].Handle.Exit.WorldPosition, Origin + (Direction * 1024))
 		return
 	end
 
-	CreatePair(Camera.CFrame.Position, Result.Position)
+	CastEffects:NewBulletHole(Result.Position, Result.Normal)
+	-- CastEffects:CreateFakeTracer(Origin, Result.Position)
+	CastEffects:CreateFakeTracer(Viewmodel["UMP-45"].Handle.Exit.WorldPosition, Result.Position)
 
-	local PartDepth, HitPosition = Caster:FindThickness(Result.Instance, Result.Position, Camera.CFrame.Position + (Camera.CFrame.LookVector * 64), -Camera.CFrame.LookVector * 64)
-	CreatePair(Camera.CFrame.Position + (Camera.CFrame.LookVector * 64), HitPosition)
+	local PartDepth, HitPosition = Caster:FindThickness(Result.Instance, Result.Position, Result.Position + (Direction * 64), -Direction * 64)
 
 	if not PartDepth then
 		return
@@ -37,6 +36,14 @@ UserInputService.InputBegan:Connect(function(input: InputObject, gameProcessedEv
 	if gameProcessedEvent then return end
 
 	if input.UserInputType == Enum.UserInputType.MouseButton1 then
-		Cast()
+		local OriginPosition: Vector3 = Camera.CFrame.Position
+		local LookVector: Vector3 = Camera.CFrame.LookVector
+
+		WeaponFire(OriginPosition, LookVector)
 	end
+end)
+
+RunService.RenderStepped:Connect(function(deltaTime)
+	Viewmodel.Parent = Camera
+	Viewmodel:SetPrimaryPartCFrame(Camera.CFrame)
 end)
