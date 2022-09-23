d3fe1751690e486f5a0beed5631f00c27721aed2
reimakesgames <reimakesgames.dev@gmail.com>
2022-09-23 13:40:16 +0800

    removed swear word !!! (roblox is going to ban me)

--- src/client/PrismaRunner.client.lua
+++ src/client/PrismaRunner.client.lua
@@ -2,5 +2,4 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Packages = ReplicatedStorage:WaitForChild("Packages")
 local Prisma = Packages:WaitForChild("prisma")
 
-print("wth")
 require(Prisma)
