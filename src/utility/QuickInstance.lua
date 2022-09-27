fae7b1d2a549876253f9d5a43d699daa8835cb7e
reimakesgames <reimakesgames.dev@gmail.com>
2022-09-27 19:25:01 +0800

    quickinstance bug fix

--- src/utility/QuickInstance.lua
+++ src/utility/QuickInstance.lua
@@ -1,13 +1,15 @@
 return function<Class>(ClassName: Class, Properties: {[string]: any}): Class
 	local Object = Instance.new(ClassName)
-	for Property, Value in Properties do
-		if Property == "Parent" then
-			continue
+	if Properties then
+		for Property, Value in Properties do
+			if Property == "Parent" then
+				continue
+			end
+			Object[Property] = Value
 		end
-		Object[Property] = Value
-	end
-	if Properties.Parent then
-		Object.Parent = Properties.Parent
+		if Properties.Parent then
+			Object.Parent = Properties.Parent
+		end
 	end
 
 	return Object
