ff8d0b026504b604f7b46223731c7b1d808d531f
rei <reimakesgames.dev@gmail.com>
2022-10-20 23:42:19 +0800

    local unit testing (#27)
    added local unit testing

---
+++ test/test.spec.lua
@@ -0,0 +1,7 @@
+return function ()
+	describe("this thing", function()
+		it("does nothing", function()
+			expect(true).to.be.ok()
+		end)
+	end)
+end
---
+++ tester.lua
@@ -0,0 +1,15 @@
+-- This is a required VSCode extension
+-- VS Marketplace Link: https://marketplace.visualstudio.com/items?itemName=discretegames.f5anything
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerStorage = game:GetService("ServerStorage")
+
+local Packages = ReplicatedStorage:WaitForChild("Packages")
+local Tests = ServerStorage:WaitForChild("Tests")
+
+local TestEZ = require(Packages.testez)
+local Bootstrapper = require(Tests.container.bootstrap)
+
+Bootstrapper:run({
+	Tests["test.spec"]
+})
