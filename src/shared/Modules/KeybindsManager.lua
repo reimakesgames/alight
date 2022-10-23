d5dfb07d72be942c434c150a2521cf6663e980fd
rei <reimakesgames.dev@gmail.com>
2022-10-23 21:28:14 +0800

    bug fixes

--- src/shared/Modules/KeybindsManager.lua
+++ src/shared/Modules/KeybindsManager.lua
@@ -1,14 +1,15 @@
 export type ActionClass = {
-	RegisterKey: (key: Enum.KeyCode | Enum.UserInputType) -> ();
+	RegisterKey: (self: ActionClass, key: Enum.KeyCode | Enum.UserInputType) -> ();
+	GetKeyType: (self: ActionClass) -> (Enum.KeyCode | Enum.UserInputType);
 
 	Name: string;
 	Key: Enum.KeyCode | Enum.UserInputType;
 }
 
 export type KeybindsManager = {
-	CreateAction: (name: string) -> (ActionClass);
+	CreateAction: (self: KeybindsManager, name: string) -> (ActionClass);
 
-	Actions: Dictionary<ActionClass>
+	Actions: Dictionary<ActionClass> | {[string]: ActionClass}
 }
 
 local KeybindsManager = {
@@ -27,19 +28,27 @@ function KeybindsManager:CreateAction(name: string): ActionClass
 		Name = name
 	}, ActionClass)
 
+	KeybindsManager.Actions[name] = Action
+
 	return Action :: ActionClass
 end
 
 function ActionClass:RegisterKey(key: Enum.KeyCode | Enum.UserInputType): nil
-	if key.EnumType ~= Enum.KeyCode or key.EnumType ~= Enum.UserInputType then
+	if key.EnumType ~= Enum.KeyCode and key.EnumType ~= Enum.UserInputType then
 		return error("provided key is not a Enum.KeyCode or Enum.UserInputType")
 	end
 
-	ActionClass.Key = key
+	self.Key = key
+end
+
+function ActionClass:GetKeyType()
+	return self.Key.EnumType
 end
 
 -- TODO: add saving
 -- TODO: create a workflow to replace everything that relies on static code
+
 -- TODO: separate KeyCodes and UserInputTypes to support two types for future proofing
+-- FIXED: can be replaced with a function returning the key type
 
 return KeybindsManager
