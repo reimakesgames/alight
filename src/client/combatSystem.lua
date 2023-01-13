b08d1595d437af797c431d216484bfb3f407b4b5
rei <reimakesgames.dev@gmail.com>
2023-01-13 22:03:08 +0800

    slow walking

--- src/client/combatSystem.lua
+++ src/client/combatSystem.lua
@@ -1,5 +1,6 @@
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local UserInputService = game:GetService("UserInputService")
 local RunService = game:GetService("RunService")
 
 local Shared = ReplicatedStorage.Shared
@@ -9,6 +10,7 @@ local Viewmodel = require(Shared.Viewmodel)
 local fastInstance = require(Shared.fastInstance)
 
 local viewmodelHandler = require(script.Parent.viewmodelHandler)
+local keybindsHandler = require(script.Parent.keybindsHandler)
 
 local Camera = workspace.CurrentCamera
 local LocalPlayer = Players.LocalPlayer
@@ -17,10 +19,14 @@ local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
 local Rig = Assets.v_UMP45
 local Arms = Assets.Arms
 
+local Walking = false
+
 local function OnCharacterAdded(character)
 	Character = character
 	viewmodelHandler.SetCharacter(character)
 end
+
+keybindsHandler:LoadKeybindsFromServer()
 
 local combatSystem = {}
 
@@ -43,11 +49,27 @@ function combatSystem.init()
 	RunService:BindToRenderStep("__VIEWMODEL__", Enum.RenderPriority.Camera.Value, function(deltaTime)
 		viewmodelHandler.Update(deltaTime, Camera)
 	end)
+	RunService:BindToRenderStep("__AFTER__", Enum.RenderPriority.Input.Value + 1, function(deltaTime)
+		local Humanoid = Character:FindFirstChildOfClass("Humanoid")
+		Humanoid.WalkSpeed = Walking and 8 or 16
+	end)
 	LocalPlayer.CharacterAdded:Connect(OnCharacterAdded)
 	viewmodelHandler.SetCharacter(Character)
 
 	viewmodelHandler.CacheViewmodel(viewmodel)
 	viewmodelHandler.SetViewmodel(viewmodel)
+	UserInputService.InputBegan:Connect(function(input, gameProcessed)
+		if gameProcessed then return end
+		if input.KeyCode == keybindsHandler.keybinds.Gameplay.Walk then
+			Walking = true
+		end
+	end)
+	UserInputService.InputEnded:Connect(function(input, gameProcessed)
+		if gameProcessed then return end
+		if input.KeyCode == keybindsHandler.keybinds.Gameplay.Walk then
+			Walking = false
+		end
+	end)
 end
 
 return combatSystem
