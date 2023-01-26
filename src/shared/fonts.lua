9173fd7e22ef0c1bc4ba929bf9406a9acc82f303
rei <reimakesgames.dev@gmail.com>
2023-01-26 19:48:14 +0800

    fonts module (for marketplace fonts)

---
+++ src/shared/fonts.lua
@@ -0,0 +1,8 @@
+return {
+	Poppins = {
+		Normal = {
+			Regular = Font.fromId(11702779409, Enum.FontWeight.Regular, Enum.FontStyle.Normal),
+			Bold = Font.fromId(11702779409, Enum.FontWeight.Bold, Enum.FontStyle.Normal),
+		}
+	}
+}
