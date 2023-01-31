75c46e2fc261385300deb3eb1c99ba12d8c27bda
rei <reimakesgames.dev@gmail.com>
2023-01-31 21:23:53 +0800

    isRealNumber bug fix

--- src/shared/isRealNumber.lua
+++ src/shared/isRealNumber.lua
@@ -1,3 +1,11 @@
+-- return function (number: number): boolean
+-- 	local isNumberType = type(number) == "number"
+-- 	local isNotNaN = (number == number)
+-- 	local isNotPositiveInf = not (number == math.huge)
+-- 	local isNotNegativeInf = not (number == -math.huge)
+-- 	return isNumberType and isNotNaN and isNotPositiveInf and isNotNegativeInf
+-- end
+
 return function (number: number): boolean
-	return not (type(number) ~= "number" and number ~= number and number == math.huge and number == -math.huge)
+	return (type(number) == "number") and (number == number) and (not (number == math.huge)) and (not (number == -math.huge))
 end
