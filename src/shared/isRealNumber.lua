1fe80cb774b1be97c97aae9475e4e75e46323cdd
rei <reimakesgames.dev@gmail.com>
2023-02-02 19:38:51 +0800

    add documentation to isRealNumber

--- src/shared/isRealNumber.lua
+++ src/shared/isRealNumber.lua
@@ -6,6 +6,9 @@
 -- 	return isNumberType and isNotNaN and isNotPositiveInf and isNotNegativeInf
 -- end
 
+--[[
+	Checks if a number is a real number, and not NaN, +inf, or -inf.
+]]
 return function (number: number): boolean
 	return (type(number) == "number") and (number == number) and (not (number == math.huge)) and (not (number == -math.huge))
 end
