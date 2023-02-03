6609fd4daf87c190cf4a90750cb497d3d9732c2f
rei <reimakesgames.dev@gmail.com>
2023-02-03 22:22:16 +0800

    utilize scene

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -7,13 +7,18 @@ BridgeNet.Start({})
 
 UserInputService.MouseBehavior = Enum.MouseBehavior.LockCenter
 
-local function Initialize(module: ModuleScript)
-	task.spawn(function()
-		require(module).init()
-	end)
-end
+-- local function Initialize(module: ModuleScript)
+-- 	task.spawn(function()
+-- 		require(module).init()
+-- 	end)
+-- end
 
-Initialize(script.performance)
-Initialize(script.changelogs)
-Initialize(script.movementHandler)
-Initialize(script.combatSystem)
+-- Initialize(script.performance)
+-- Initialize(script.changelogs)
+-- Initialize(script.movementHandler)
+-- Initialize(script.combatSystem)
+
+local Scene = require(ReplicatedStorage.Shared.Scene)
+local NewScene = Scene.new(workspace.World)
+
+NewScene:RunScene(true)
